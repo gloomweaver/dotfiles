7b5eb714268acf3947a2f0e0d4b9f812bc665021
Kirill Khramkov <hramkir@gmail.com>
2023-05-02 07:49:12 +0400

    Remove dw binding

--- .config/nvim/lua/maps.lua
+++ .config/nvim/lua/maps.lua
@@ -3,8 +3,6 @@ keymap.set('n', 'x', '"_x')
 -- Increment/decrement
 keymap.set('n', '+', '<C-a>')
 keymap.set('n', '-', '<C-x>')
--- Delete a word backwards
-keymap.set('n', 'dw', 'vb"_d')
 -- Select all
 keymap.set('n', '<C-a>', 'gg<S-v>G')
 -- Save with root permission (not working for now)
