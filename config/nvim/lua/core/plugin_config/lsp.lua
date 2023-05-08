0e98a7cde9a10f3b588a8b38b467a8536690a08f
Kirill Khramkov <hramkir@gmail.com>
2023-05-08 13:11:58 +0400

    Configure autocomplete

--- config/nvim/lua/core/plugin_config/lsp.lua
+++ config/nvim/lua/core/plugin_config/lsp.lua
@@ -1,11 +1,19 @@
+local autocomplete = require('core.plugin_config.autocomplete')
+
 require('mason').setup()
 require('mason-lspconfig').setup({
-    ensure_installed = {"lua_ls", "elixirls"}
+    ensure_installed = {"lua_ls", "elixirls", "eslint_d", "prettierd", "tsserver"}
 })
 
-require('lspconfig').lua_ls.setup {}
+require('lspconfig').lua_ls.setup {
+    capablities = autocomplete.capablities
+}
 require('lspconfig').elixirls.setup {
     elixirLS = {
         dialyzerAnabled = true
-    }
+    },
+    capablities = autocomplete.capablities
+}
+require('lspconfig').tsserver.setup {
+    capablities = autocomplete.capablities
 }
