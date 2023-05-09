f764ed5d8e9b9e668f190401b0275f6cfdc393dd
Kirill Khramkov <hramkir@gmail.com>
2023-05-09 17:17:26 +0400

    format

--- config/nvim/lua/core/plugin_config/lsp.lua
+++ config/nvim/lua/core/plugin_config/lsp.lua
@@ -3,28 +3,28 @@ local cmp = require("cmp")
 
 lsp.preset("recommended")
 
-lsp.ensure_installed({"tsserver", "eslint_d", "prettierd", "lua_ls", "elixirls", "rust_analyzer"})
+lsp.ensure_installed({ "tsserver", "eslint_d", "prettierd", "lua_ls", "elixirls", "rust_analyzer" })
 
 lsp.on_attach(function(client, bufnr)
-    lsp.default_keymaps({
-        buffer = bufnr
-    })
+	lsp.default_keymaps({
+		buffer = bufnr,
+	})
 
-    vim.keymap.set({'n', 'x'}, 'gq', function()
-        vim.lsp.buf.format({
-            async = false,
-            timeout_ms = 10000
-        })
-    end)
+	vim.keymap.set({ "n", "x" }, "gq", function()
+		vim.lsp.buf.format({
+			async = false,
+			timeout_ms = 10000,
+		})
+	end)
 end)
 
 lsp.format_on_save({
-    format_opts = {
-        timeout_ms = 10000
-    },
-    servers = {
-        ["null-ls"] = {"javascript", "typescript", "lua"}
-    }
+	format_opts = {
+		timeout_ms = 10000,
+	},
+	servers = {
+		["null-ls"] = { "javascript", "typescript", "lua" },
+	},
 })
 
 lsp.setup()
@@ -32,15 +32,18 @@ lsp.setup()
 local null_ls = require("null-ls")
 
 null_ls.setup({
-    sources = {null_ls.builtins.formatting.prettierd, null_ls.builtins.diagnostics.eslint_d,
-               null_ls.builtins.formatting.stylua}
+	sources = {
+		null_ls.builtins.formatting.prettierd,
+		null_ls.builtins.diagnostics.eslint_d,
+		null_ls.builtins.formatting.stylua,
+	},
 })
 
 cmp.setup({
-    mapping = {
-        -- `Enter` key to confirm completion
-        ["<CR>"] = cmp.mapping.confirm({
-            select = false
-        })
-    }
+	mapping = {
+		-- `Enter` key to confirm completion
+		["<CR>"] = cmp.mapping.confirm({
+			select = false,
+		}),
+	},
 })
